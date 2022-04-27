71f53d19ddbc4209834e7ccdad1674ed9a7cfde9
glpat-KDiotfmiMEnKvyyL4Dpr <glpat-KDiotfmiMEnKvyyL4Dpr>
2022-04-27 10:34:36 +0530

    New files added

---
+++ OnlineShop/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,179 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using OnlineShop.Data;
+using OnlineShop.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private ApplicationDbContext _db;
+        [Obsolete]
+        private IHostingEnvironment _he;
+
+        [Obsolete]
+        public ProductController(ApplicationDbContext db,IHostingEnvironment he)
+        {
+            _db = db;
+            _he = he;
+        }
+        public IActionResult Index()
+        {
+            return View(_db.Products.Include(c=>c.ProductTypes).Include(f=>f.SpecialTags).ToList());
+        }
+
+        //POST Index Action Method
+        [HttpPost]
+        public IActionResult Index(decimal? lowAmount,decimal? largeAmount)
+        {
+            var products = _db.Products.Include(c => c.ProductTypes).Include(c => c.SpecialTags).Where(c => c.Price >= lowAmount && c.Price <= largeAmount).ToList();
+            if(lowAmount==null || largeAmount==null)
+            {
+                 products = _db.Products.Include(c => c.ProductTypes).Include(c => c.SpecialTags).ToList();
+            }
+            return View(products);
+        }
+
+        //GET Create Method
+        public IActionResult Create()
+        {
+            ViewData["productTypeId"] = new SelectList(_db.ProductTypes.ToList(), "Id", "ProductType");
+            ViewData["TagId"] = new SelectList(_db.SpecialTags.ToList(), "Id", "SpecialTag");
+            return View();
+        }
+
+        //POST Create Method
+        [HttpPost]
+        [Obsolete]
+        public async Task<IActionResult> Create(Products products,IFormFile image)
+        {
+            if(ModelState.IsValid)
+            {
+                var searchProduct = _db.Products.FirstOrDefault(c => c.Name == products.Name);
+                if(searchProduct!=null)
+                {
+                    ViewBag.message = "This product is already exist";
+                    ViewData["productTypeId"] = new SelectList(_db.ProductTypes.ToList(), "Id", "ProductType");
+                    ViewData["TagId"] = new SelectList(_db.SpecialTags.ToList(), "Id", "SpecialTag");
+                    return View(products);
+                }
+
+                if(image!=null)
+                {
+                    var name = Path.Combine(_he.WebRootPath + "/Images", Path.GetFileName(image.FileName));
+                    await image.CopyToAsync(new FileStream(name, FileMode.Create));
+                    products.Image ="Images/" + image.FileName;
+                }
+                if(image==null)
+                {
+                    products.Image = "Images/noimagefound.png";
+                }
+                _db.Products.Add(products);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(products);
+        }
+
+        //GET Edit Action Method
+        public ActionResult Edit(int? id)
+        {
+            ViewData["productTypeId"] = new SelectList(_db.ProductTypes.ToList(), "Id", "ProductType");
+            ViewData["TagId"] = new SelectList(_db.SpecialTags.ToList(), "Id", "SpecialTag");
+            if(id==null)
+            {
+                return NotFound();
+            }
+            var product = _db.Products.Include(c => c.ProductTypes).Include(c => c.SpecialTags).FirstOrDefault(c => c.Id == id);
+            if(product==null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        //POST Edit Action Method
+        [HttpPost]
+        [Obsolete]
+        public async Task<IActionResult> Edit(Products products, IFormFile image)
+        {
+            if (ModelState.IsValid)
+            {
+                if (image != null)
+                {
+                    var name = Path.Combine(_he.WebRootPath + "/Images", Path.GetFileName(image.FileName));
+                    await image.CopyToAsync(new FileStream(name, FileMode.Create));
+                    products.Image = "Images/" + image.FileName;
+                }
+
+                if (image == null)
+                {
+                    products.Image = "Images/noimage.PNG";
+                }
+                _db.Products.Update(products);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(products);
+        }
+
+        //GET Details Action Method
+        public ActionResult Details(int? id)
+        {
+            if(id==null)
+            {
+                return NotFound();
+            }
+            var product = _db.Products.Include(c => c.ProductTypes).Include(c => c.SpecialTags).FirstOrDefault(c => c.Id == id);
+            if(product==null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        //GET Delete Action Method
+        public ActionResult Delete(int? id)
+        {
+            if(id==null)
+            {
+                return NotFound();
+            }
+            var product = _db.Products.Include(c=>c.SpecialTags).Include(c=>c.ProductTypes).Where(c => c.Id == id).FirstOrDefault();
+            if(product==null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        //POST Delete Action Method
+        [HttpPost]
+        [ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirm(int? id)
+        {
+            if(id==null)
+            {
+                return NotFound();
+            }
+            var product = _db.Products.FirstOrDefault(c => c.Id == id);
+            if(product==null)
+            {
+                return NotFound();
+            }
+            _db.Products.Remove(product);
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ OnlineShop/Areas/Admin/Controllers/SpecialTagsController.cs
@@ -0,0 +1,148 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Data;
+using OnlineShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SpecialTagsController : Controller
+    {
+        private ApplicationDbContext _db;
+        public SpecialTagsController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View(_db.SpecialTags.ToList());
+        }
+
+        //Create Get Action Method
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //Create Post Action Method
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(SpecialTags specialTags)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.SpecialTags.Add(specialTags);
+                await _db.SaveChangesAsync();
+                TempData["save"] = "Product Type has been saved successfully!!!";
+                return RedirectToAction(nameof(Index));
+            }
+            return View(specialTags);
+
+        }
+
+        //Edit Get Action Method
+        public ActionResult Edit(int? id)
+        {
+            if(id==null)
+            {
+                return NotFound();
+            }
+            var specialTag = _db.SpecialTags.Find(id);
+            if(specialTag==null)
+            {
+                return NotFound();
+            }
+            return View(specialTag);
+        }
+
+        //Edit Post Action Method
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(SpecialTags specialTags)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Update(specialTags);
+                await _db.SaveChangesAsync();
+                TempData["save"] = "Product Type has been updated successfully!!!";
+                return RedirectToAction(nameof(Index));
+            }
+            return View(specialTags);
+
+        }
+
+        //Details Get Action Method
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var specialTag = _db.SpecialTags.Find(id);
+            if (specialTag == null)
+            {
+                return NotFound();
+            }
+            return View(specialTag);
+        }
+
+        //Details Post Action Method
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Details(SpecialTags specialTags)
+        {
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        //Delete Get Action Method
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var specialTag = _db.SpecialTags.Find(id);
+            if (specialTag == null)
+            {
+                return NotFound();
+            }
+            return View(specialTag);
+        }
+
+        //Delete Post Action Method
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int? id, SpecialTags specialTags)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            if (id != specialTags.Id)
+            {
+                return NotFound();
+            }
+
+            var specialTag = _db.SpecialTags.Find(id);
+            if (specialTag == null)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Remove(specialTag);
+                await _db.SaveChangesAsync();
+                TempData["save"] = "Deleted successfully!!!";
+                return RedirectToAction(nameof(Index));
+            }
+            return View(specialTags);
+
+        }
+    }
+}
